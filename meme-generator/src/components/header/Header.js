b68c3600864542da6f6d77e1c4dbf015561cce63
vivian <vivian.v18723@gmail.com>
2022-01-17 22:36:21 +0000

    Refactor styles and replace image with icon.

--- meme-generator/src/components/header/Header.js
+++ meme-generator/src/components/header/Header.js
@@ -1,17 +1,13 @@
-import troll from "../../images/troll-face.png";
-
 const Header = () => {
   return (
     <header className="header-container">
-      <div className="troll-container">
-        <figure>
-          <img src={troll} alt="A troll face." className="header-img" />
-        </figure>
-        <h1 className="title">Meme Generator</h1>
+      <div className="face-container">
+        <i class="far fa-grin-tongue-wink"></i>
+        <h1 className="main-title">Meme Generator</h1>
       </div>
 
       <div className="react-course-container">
-        <p>React Course - Project 3</p>
+        <p className="sub-title">React Course - Project 3</p>
       </div>
     </header>
   );
