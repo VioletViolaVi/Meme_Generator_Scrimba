6a9943a2aa5218186ee292af3b047c36d038d3be
vivian <vivian.v18723@gmail.com>
2022-01-17 23:58:21 +0000

    Add style to form.

--- meme-generator/src/components/main/Meme.js
+++ meme-generator/src/components/main/Meme.js
@@ -1,9 +1,19 @@
 const Meme = () => {
   return (
-    <form method="get">
-      <input type="text" placeholder="First Part" required />
-      <input type="text" placeholder="Second Part" required />
-      <button type="submit">
+    <form method="get" className="meme-form">
+      <input
+        required
+        type="text"
+        placeholder="First Part"
+        className="meme-input1"
+      />
+      <input
+        required
+        type="text"
+        placeholder="Second Part"
+        className="meme-input2"
+      />
+      <button type="submit" className="meme-btn">
         Get a new meme image <i className="fas fa-image"></i>
       </button>
     </form>
