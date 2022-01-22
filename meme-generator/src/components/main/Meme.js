5758d73dcfdb5144901bb43e6a7efff26f4054a5
vivian <vivian.v18723@gmail.com>
2022-01-22 22:08:59 +0000

    Add ability to set and change state of image.

--- meme-generator/src/components/main/Meme.js
+++ meme-generator/src/components/main/Meme.js
@@ -1,14 +1,23 @@
 import Data from "./Data";
+import React, { useState } from "react";
 
 const Meme = () => {
+  // set state OG of meme via deconstructing
+  const [meme, setMeme] = useState("https://i.imgflip.com/30b1gx.jpg");
+
   const getNewMemeImg = () => {
     // gets memes array
     const memeArr = Data.data.memes;
     // gets random nu 1-99
     const randomNum = Math.floor(Math.random() * 100);
     // gets .url from memes array
-    const { url } = memeArr[randomNum];
-    console.log(url);
+    const { randomUrl } = memeArr[randomNum];
+    console.log(randomUrl);
+
+    // change state of meme
+    setMeme((prevMeme) => {
+      return (prevMeme = randomUrl);
+    });
   };
 
   return (
@@ -28,6 +37,8 @@ const Meme = () => {
       <button type="submit" className="meme-btn" onClick={getNewMemeImg}>
         Get a new meme image <i className="fas fa-image"></i>
       </button>
+
+      <img src={meme} alt="Random from Data.js file." />
     </section>
   );
 };
